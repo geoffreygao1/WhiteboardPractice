7db2afd78a5f31b85ef96751557b85a010daa04b
Geoffrey Gao <geoffreygao1@gmail.com>
2023-05-17 11:59:02 +0900

    Project Euler 1

--- ProjectEuler.js
+++ ProjectEuler.js
@@ -4,3 +4,13 @@ Solve Problem 1 on Project Euler using a functional approach. Here is the proble
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000. */
+
+const problemOne = (inputNum) => {
+  let result = 0;
+  for (let i = 0; i < inputNum; i++) {
+    if ((i % 3 == 0) || (i % 5 == 0)) {
+      result += i;
+    }
+  }
+  return result;
+}
